478c5d9a31e5a2cd40babee19352ca55869c605b
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-01-19 12:12:11 +0000

    first few lessons commited

---
+++ hello.py
@@ -0,0 +1,11 @@
+failed_subjects = 2.56
+name='Eliska'
+print('Dear Mrs Mooney')
+print(' Your daughter ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
+print(name + ' will need to redo ' + str(failed_subjects) + ' courses.')
+name="Harry"
+print(name + ' is doing well in geography.')
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
